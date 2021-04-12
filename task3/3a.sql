942a605e226a8fdda71dba8c55f62bac82244b74
JohnVonNeumann <louiswillcock@gmail.com>
2021-04-12 15:33:58 +1000

    CREATE task3/3a.sql - add script to select by ascedning relyear

---
+++ task3/3a.sql
@@ -0,0 +1,11 @@
+/*
+ * 3a.sql
+ * Copyright (C) 2021 lw <lw@diyu>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+SELECT 'xxxxxxxxx' AS StuId, title, runtime, relyear FROM movie7949 ORDER BY runtime ASC;
+
+
+-- vim:et
