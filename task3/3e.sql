96f528ce1ee91f08ba7b0003ff62dd27573d47c6
JohnVonNeumann <louiswillcock@gmail.com>
2021-04-13 09:36:54 +1000

    CREATE 3e.sql - find movies by a list of movieno's

---
+++ task3/3e.sql
@@ -0,0 +1,10 @@
+/*
+ * 3e.sql
+ * Copyright (C) 2021 lw <lw@diyu>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+SELECT '7949' as StuId, movie7949.* FROM movie7949 WHERE movieno IN (98, 392044, 36557, 557) ORDER BY movieno ASC;
+
+-- vim:et
